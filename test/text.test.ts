06d5e5d74d8287535d2e50dec1edf0c3a1c69bf0
ky <y.keddaoui@gmail.com>
2021-07-10 09:13:55 +0100

    added update text test

--- test/text.test.ts
+++ test/text.test.ts
@@ -1,5 +1,6 @@
-import request from 'supertest'
+import request, { Test } from 'supertest'
 import app from '../src/app'
+import { TextDocument } from '../src/models/Text'
 
 describe('POST /text', () => {
   test('should respond with a 201 status code', async () => {
@@ -25,6 +26,50 @@ describe('POST /text', () => {
   test('should respond with a 500 status code when body content is not valid', async () => {
     const response = await request(app).post('/text').send({})
     expect(response.statusCode).toBe(500)
+  })
+})
+
+describe('PUT /text', () => {
+  let text: TextDocument
+
+  beforeAll(() =>
+    request(app)
+      .post('/text')
+      .send({
+        ar: {
+          title: 'اللوائح المالية 2021',
+          content: 'هذا هو محتوى اللوائح المالية 2021',
+        },
+        fr: {
+          title: 'lois des finance 2021',
+          content: 'le tesxt de lois de finances 2021',
+        },
+        en: {
+          title: 'Finances regulations 2021',
+          content: 'this is the content of finances regulations 2021',
+        },
+      })
+      .then(res => {
+        text = res.body
+      })
+  )
+
+  test('should respond with a 200 status code', async () => {
+    const response = await request(app)
+      .put('/text')
+      .send({
+        _id: text._id,
+        fr: {
+          title: 'Finances regulations 2022',
+          content: 'test content',
+        },
+      })
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('should respond with a 400 status code when body content is not valid', async () => {
+    const response = await request(app).put('/text').send({})
+    expect(response.statusCode).toBe(400)
   })
 })
 
